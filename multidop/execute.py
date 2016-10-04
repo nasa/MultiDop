af8408370a7b3e61a0f6f773823a5dc3b68fd234
Timothy Lang <timothy.j.lang@nasa.gov>
2016-10-04 10:09:40 -0500

    modified:   README 	modified:   examples/MultiDop_Sample_Workflow.ipynb 	deleted:    man/man3/geog_lib.3 	deleted:    man/man3/geog_proj.3 	deleted:    man/man3/nnetcdf.3 	modified:   multidop/execute.py

--- multidop/execute.py
+++ multidop/execute.py
@@ -2,18 +2,21 @@ from __future__ import print_function
 import subprocess
 import shlex
 import os
+import sys
 
 
 def run_command(command):
     """
     This function captures text output from a command-line program and
     displays it in the Python shell. Currently does not print the output
-    unitl after the command has completed.
+    until after the command has completed.
     """
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE,
                                bufsize=0)
     while True:
         output = process.stdout.readline()
+        if sys.version_info >= (3, 0, 0):
+            output = output.decode('utf-8')
         if output == '' and process.poll() is not None:
             break
         if output:
